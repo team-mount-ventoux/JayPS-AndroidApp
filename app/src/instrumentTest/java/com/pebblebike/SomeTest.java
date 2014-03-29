e3c6e2915cfd923b41ce169e941fa4a6d1b83356
Nic Jackson <nic@thatlondon.com>
2014-03-29 22:18:57 +0000

    base gradle build

---
+++ app/src/instrumentTest/java/com/pebblebike/SomeTest.java
@@ -0,0 +1,14 @@
+package com.pebblebike;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import junit.framework.TestCase;
+
+public class SomeTest extends TestCase {
+
+    @SmallTest
+    public void testSomething() {
+        assertEquals(true,true);
+    }
+
+}
