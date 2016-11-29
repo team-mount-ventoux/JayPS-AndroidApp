0cbeacfe2d0d1031933702ee23a23dc15c2c71d1
JayPS <dev@jayps.fr>
2016-11-29 22:25:44 +0100

    Rework NewLocationToPebbleDictionary to use putData*

--- app/src/main/java/com/njackson/adapters/NewLocationToPebbleDictionary.java
+++ app/src/main/java/com/njackson/adapters/NewLocationToPebbleDictionary.java
@@ -97,62 +97,30 @@ public class NewLocationToPebbleDictionary extends PebbleDictionary{
         }
         data[BYTE_SETTINGS] += (byte) ((refresh_code % 4) * (1<<POS_REFRESH)); // 2 bits
 
-        data[BYTE_ACCURACY] = (byte) Math.ceil(event.getAccuracy());
+        putDataUInt8(data, BYTE_ACCURACY, (int) Math.ceil(event.getAccuracy()));
+        putDataUInt16(data, BYTE_DISTANCE1, (int) (Math.floor(100 * event.getDistance())));
+        putDataUInt16(data, BYTE_TIME1, event.getElapsedTimeSeconds());
+        putDataUInt16(data, BYTE_ALTITUDE1, (int) event.getAltitude());
+        putDataInt16(data, BYTE_ASCENT1, (int) event.getAscent());
+        putDataInt16(data, BYTE_ASCENTRATE1, (int) event.getAscentRate());
+        putDataInt8(data, BYTE_SLOPE, (int) (event.getSlope()));
+        putDataInt16(data, BYTE_XPOS1, (int) event.getXpos());
+        putDataInt16(data, BYTE_YPOS1, (int) event.getYpos());
+        putDataUInt16(data, BYTE_SPEED1, (int) Math.floor(10 * event.getSpeed()));
+        putDataUInt8(data, BYTE_BEARING, (int)  (event.getBearing() / 360 * 256));
 
-        data[BYTE_DISTANCE1] = (byte) (((int) (Math.floor(100 * event.getDistance()) / 1)) % 256);
-        data[BYTE_DISTANCE2] = (byte) (((int) (Math.floor(100 * event.getDistance()) / 1)) / 256);
-
-        data[BYTE_TIME1] = (byte) ((event.getElapsedTimeSeconds()) % 256);
-        data[BYTE_TIME2] = (byte) ((event.getElapsedTimeSeconds()) / 256);
-
-        data[BYTE_ALTITUDE1] = (byte) ((event.getAltitude()) % 256);
-        data[BYTE_ALTITUDE2] = (byte) ((event.getAltitude()) / 256);
-
-        data[BYTE_ASCENT1] = (byte) (Math.abs(event.getAscent()) % 256);
-        data[BYTE_ASCENT2] = (byte) ((Math.abs(event.getAscent()) / 256) % 128);
-        if (event.getAltitude() < 0) {
-            data[BYTE_ASCENT2] += 128;
-        }
-
-        data[BYTE_ASCENTRATE1] = (byte) (Math.abs(event.getAscentRate()) % 256);
-        data[BYTE_ASCENTRATE2] = (byte) ((Math.abs(event.getAscentRate()) / 256) % 128);
-        if (event.getAscentRate() < 0) {
-            data[BYTE_ASCENTRATE2 ] += 128;
-        }
-
-        data[BYTE_SLOPE] = (byte) ((Math.abs(event.getSlope())) % 128);
-        if (event.getSlope() < 0) {
-            data[BYTE_SLOPE] += 128;
-        }
-
-        data[BYTE_XPOS1] = (byte) (Math.abs(event.getXpos()) % 256);
-        data[BYTE_XPOS2] = (byte) (Math.abs(event.getXpos() / 256) % 128);
-        if (event.getXpos() < 0) {
-            data[BYTE_XPOS2] += 128;
-        }
-        data[BYTE_YPOS1] = (byte) (Math.abs(event.getYpos()) % 256);
-        data[BYTE_YPOS2] = (byte) ((Math.abs(event.getYpos()) / 256) % 128);
-        if (event.getYpos() < 0) {
-            data[BYTE_YPOS2] += 128;
-        }
-
-        data[BYTE_SPEED1] = (byte) (((int) (Math.floor(10 * event.getSpeed()) / 1)) % 256);
-        data[BYTE_SPEED2] = (byte) (((int) (Math.floor(10 * event.getSpeed()) / 1)) / 256);
-        data[BYTE_BEARING] = (byte) (((int)  (event.getBearing() / 360 * 256)) % 256);
-
-        data[BYTE_HEARTRATE] = (byte) (event.getHeartRate() % 256);
+        putDataUInt8(data, BYTE_HEARTRATE, event.getHeartRate());
         if (location_data_version >= Constants.PEBBLE_LOCATION_DATA_V3) {
-            data[BYTE_CADENCE] = (byte) (event.getCyclingCadence() % 256);
+            putDataUInt8(data, BYTE_CADENCE, event.getCyclingCadence());
         } else {
             // old protocol, only one field (BYTE_HEARTRATE) for both hr and cadence
             if (event.getCyclingCadence() < 255) {
                 // CSC sensor is configured and cadence is received, sent it instead of hr (both are not supported yet at the same time)
-                data[BYTE_HEARTRATE] = (byte) (event.getCyclingCadence() % 256);
+                putDataUInt8(data, BYTE_HEARTRATE, event.getCyclingCadence());
             }
         }
 
-        data[BYTE_MAXSPEED1] = (byte) (((int) (Math.floor(10 * event.getMaxSpeed()) / 1)) % 256);
-        data[BYTE_MAXSPEED2] = (byte) (((int) (Math.floor(10 * event.getMaxSpeed()) / 1)) / 256);
+        putDataUInt16(data, BYTE_MAXSPEED1, (int) (Math.floor(10 * event.getMaxSpeed())));
 
         this.addBytes(location_data_version, data);
 
@@ -167,25 +135,23 @@ public class NewLocationToPebbleDictionary extends PebbleDictionary{
         }
         if (event.getHeartRateMax() != 0) {
             byte[] data_heartmax = new byte[2];
-            data_heartmax[0] = (byte) (event.getHeartRateMax() % 256);
-            data_heartmax[1] = (byte) (event.getHeartRateMode() % 256);
+            putDataUInt8(data_heartmax, 0, event.getHeartRateMax());
+            putDataUInt8(data_heartmax, 1, event.getHeartRateMode());
             this.addBytes(Constants.MSG_HR_MAX, data_heartmax);
         }
         if (event.getSendNavigation()) {
             byte[] data_navigation = new byte[NAV_NB_BYTES];
 
             // in m, 0-65.535km
-            data_navigation[NAV_BYTE_DISTANCE1] = putDataUInt16_1((int) navigator.getNextDistance(event.getUnits()));
-            data_navigation[NAV_BYTE_DISTANCE2] = putDataUInt16_2((int) navigator.getNextDistance(event.getUnits()));
+            putDataUInt16(data_navigation, NAV_BYTE_DISTANCE1, (int) navigator.getNextDistance(event.getUnits()));
 
             // in 0.01km, 0-655km
-            data_navigation[NAV_BYTE_DTD1] = putDataUInt16_1((int) (Math.floor(navigator.getDistanceToDestination(event.getUnits()) * 100) / 1));
-            data_navigation[NAV_BYTE_DTD2] = putDataUInt16_2((int) (Math.floor(navigator.getDistanceToDestination(event.getUnits()) * 100) / 1));
+            putDataUInt16(data_navigation, NAV_BYTE_DTD1, (int) (Math.floor(navigator.getDistanceToDestination(event.getUnits()) * 100) / 1));
 
-            data_navigation[NAV_BYTE_BEARING] = putData((int) (navigator.getNextBearing() / 360 * 256));
+            putDataUInt8(data_navigation, NAV_BYTE_BEARING, (int) (navigator.getNextBearing() / 360 * 256));
 
             // in 10m, 0-2.56km
-            data_navigation[NAV_BYTE_ERROR] = putData((int) (Math.floor(Math.abs(navigator.getError()) / 10) / 1));
+            putDataUInt8(data_navigation, NAV_BYTE_ERROR, (int) (Math.floor(Math.abs(navigator.getError()) / 10)));
 
             int curPageNumber = (int) Math.floor(navigator.getNextIndex() / NB_POINTS_PER_PAGE);
             int firstPageNumberSent = Math.max(0, (curPageNumber - 1));
@@ -193,12 +159,10 @@ public class NewLocationToPebbleDictionary extends PebbleDictionary{
             //Log.d(TAG, "nextIndex:" + navigator.getNextIndex() + " curPageNumber:" + curPageNumber + " firstPageNumberSent:" + firstPageNumberSent + " firstIndex:" + firstIndex);
 
             // 0-256 pages (=> 5*256=1280 points)
-            data_navigation[NAV_BYTE_NB_PAGES] = putData((int) Math.ceil(navigator.getNbPoints() / NB_POINTS_PER_PAGE));
-            data_navigation[NAV_BYTE_PAGE_NUMBER] = putData(firstPageNumberSent);
+            putDataUInt8(data_navigation, NAV_BYTE_NB_PAGES, (int) Math.ceil(navigator.getNbPoints() / NB_POINTS_PER_PAGE));
+            putDataUInt8(data_navigation, NAV_BYTE_PAGE_NUMBER, firstPageNumberSent);
 
-            data_navigation[NAV_BYTE_NEXT_INDEX1] = putDataUInt16_1(navigator.getNextIndex());
-            data_navigation[NAV_BYTE_NEXT_INDEX2] = putDataUInt16_2(navigator.getNextIndex());
-
+            putDataUInt16(data_navigation, NAV_BYTE_NEXT_INDEX1, navigator.getNextIndex());
 
             data_navigation[NAV_BYTE_SETTINGS] += (byte) ((nav_notification ? 1: 0) * (1<<NAV_POS_NOTIFICATION));
 
@@ -216,31 +180,33 @@ public class NewLocationToPebbleDictionary extends PebbleDictionary{
                     ypos = _firstLocation.distanceTo(point) * Math.cos(_firstLocation.bearingTo(point) / 180 * 3.1415);
                     ypos = Math.floor(ypos / 10);
                 }
-
-                //use putDataInt16_1
-                data_navigation[NAV_BYTES_POINTS + 4 * i] = (byte) (Math.abs(xpos) % 256);
-                data_navigation[NAV_BYTES_POINTS + 1 + 4 * i] = (byte) (Math.abs(xpos / 256) % 128);
-                if (xpos < 0) {
-                    data_navigation[NAV_BYTES_POINTS + 1 + 4 * i] += 128;
-                }
-                data_navigation[NAV_BYTES_POINTS + 2 + 4 * i] = (byte) (Math.abs(ypos) % 256);
-                data_navigation[NAV_BYTES_POINTS + 3 + 4 * i] = (byte) ((Math.abs(ypos) / 256) % 128);
-                if (ypos < 0) {
-                    data_navigation[NAV_BYTES_POINTS + 3 + 4 * i] += 128;
-                }
+                putDataInt16(data_navigation, NAV_BYTES_POINTS + 4 * i, (int) xpos);
+                putDataInt16(data_navigation, NAV_BYTES_POINTS + 2 + 4 * i, (int) ypos);
                 //Log.d(TAG, i + " xpos:" + xpos + " ypos:" + ypos);
             }
 
             this.addBytes(Constants.MSG_NAVIGATION, data_navigation);
         }
     }
-    static byte putDataUInt16_1(int data) {
-        return (byte) (data % 256);
+
+    static void putDataInt16(byte[] data, int index, int value) {
+        data[index] = (byte) Math.abs(value % 256);
+        data[index + 1] = (byte) ((Math.abs(value) / 256) % 128);
+        if (value < 0) {
+            data[index + 1] += 128;
+        }
     }
-    static byte putDataUInt16_2(int data) {
-        return (byte) (data / 256);
+    static void putDataUInt16(byte[] data, int index, int value) {
+        data[index] = (byte) (value % 256);
+        data[index + 1] = (byte) (value / 256);
     }
-    static byte putData(int data) {
-        return (byte) (data % 256);
+    static void putDataInt8(byte[] data, int index, int value) {
+        data[index] = (byte) Math.abs(value % 128);
+        if (value < 0) {
+            data[index + 1] += 128;
+        }
+    }
+    static void putDataUInt8(byte[] data, int index, int value) {
+        data[index] = (byte) (value % 256);
     }
 }
